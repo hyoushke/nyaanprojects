d9f570a4a2d3601766f2afa56397aeae17cf8447
hyoushke <hyoushke@gmail.com>
2019-08-12 07:11:30 -0700

    Initial segregation of data based on EDI sections

--- EdiMicroservice/EdiMicroservice.Api/Controllers/EdiParserController.cs
+++ EdiMicroservice/EdiMicroservice.Api/Controllers/EdiParserController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+
+
 namespace EdiMicroservice.Api.Controllers
 {
     [Route("api/[controller]")]
@@ -49,12 +51,122 @@ namespace EdiMicroservice.Api.Controllers
 
             IList<string> z = a.Split("!");
             string gg = "";
+            /*
+                        IBillTo BillTo = new IBillTo
+                        {
+                            string Address1 = "",
+                            string Address2 = ""
+
+                        }
+            */
 
 
-            return gg;
+            //Get ISA
+            IList<string> ISA = a.Replace("!", "___!").Split("___").Where(e => e.Contains("ISA")).ToList();
+
+            //Get GS
+            IList<string> GS = a.Replace("!", "___!").Split("___").Where(e => e.Contains("!GS")).ToList();
+
+            //Get ST
+            IList<string> ST = a.Replace("!", "___!").Split("___").Where(e => e.Contains("!ST")).ToArray()
+
+            //Get BEG
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!BEG")).ToArray()
+
+
+//Get Currency
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!CUR")).ToArray()
+
+
+//GET ITD
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!ITD")).ToArray()
+
+//GET TD5
+//Carrier Details
+            "!TD5*B***ZZ*Contact buyer for shipping ins" +
+                "!TD5*B***ZZ*tructions" +
+
+
+
+a.Replace("!", "___!").Split("___").Where(e => e.Contains("!TD5")).ToArray()
+
+//GET N9
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!N9")).ToArray()
+
+//Get MSG
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!MSG")).ToArray()
+
+//Get all N1's with child n2 and n3
+//you can extract each n1's children using this query
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!N")).ToArray().Join("").Replace("!N1", "__!N1").Split("__").ToList();
+
+            //GET all N1's only
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!N")).ToArray().Join("").Replace("!N1", "__!N1").Split("__").Where(n => n.Contains("N1"));
+
+
+            //GET ALL PO1
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!PO1")).ToArray()
+
+//GET ALL PID
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!PID")).ToArray()
+
+//GET ALL DTM
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!DTM")).ToArray()
+
+
+//GET ALL CTT
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!CTT")).ToArray()
+
+//GET ALL AMT
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!AMT")).ToArray()
+
+//GET ALL SE
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!SE")).ToArray()
+
+//GET ALL IEA
+            a.Replace("!", "___!").Split("___").Where(e => e.Contains("!IEA")).ToArray()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return a;
 
         }
 
 
     }
 }
+
---
+++ EdiMicroservice/EdiMicroservice.Api/Controllers/IBillTo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdiMicroservice.Api.Controllers
+{
+    public interface IBillTo
+    {
+        string Address1 { get; set; };
+        string Address2 { get; set; };
+
+
+
+    }
+}
